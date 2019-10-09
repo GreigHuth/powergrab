9ef1c34e3cfc8d5b6eb5ebb41a539b6316462692
Greigashaska <greighuth@hotmail.co.uk>
2019-10-09 13:37:07 +0100

    Part 1 complete

--- src/main/java/uk/ac/ed/inf/powergrab/Direction.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -13,25 +13,28 @@ public enum Direction {
 	// 		distances for every direction
 	
 	 
-	// D(width, height)
+	// <direction>(width, height)
 	// the width is amount to add to the longitude to get the new position
+	// the height is the amount to add to the latitude to get the new position
 	N(0,  0.0003),
-	NNE(0.0003*cos(toRadians(67.5)) ,0.003*sin(toRadians(67.5)) ),
-	NE(0.0003*cos(toRadians(45)) , 0.003*sin(toRadians(45))),
-	ENE(0.0003*cos(toRadians(22.5)),0.003*sin(toRadians(22.5))),
+	NNE(0.0003*cos(toRadians(67.5)) ,0.0003*sin(toRadians(67.5)) ),
+	NE(0.0003*cos(toRadians(45)) , 0.0003*sin(toRadians(45))),
+	ENE(0.0003*cos(toRadians(22.5)),0.0003*sin(toRadians(22.5))),
 	E(0.0003, 0),
 	
-	ESE(0,-0),
-	SE(0,-0),
-	SSE(0,-0),
-	S(0,-0),
-	SSW(-0,-0),
-	SW(-0,-0),
-	WSW(-0,-0),
-	W(-0,0),
-	WNW(-0,0),
-	NW(-0,0),
-	NNW(-0,0);
+	ESE(0.0003*cos(toRadians(22.5)),-0.0003*sin(toRadians(22.5))),
+	SE(0.0003*cos(toRadians(45)),-0.0003*sin(toRadians(45))),
+	SSE(0.0003*cos(toRadians(67.5)),-0.0003*sin(toRadians(67.5))),
+	S(0,-0.0003),
+	
+	SSW(-0.0003*cos(toRadians(67.5)),-0.0003*sin(toRadians(67.5))),
+	SW(-0.0003*cos(toRadians(45)),-0.0003*sin(toRadians(45))),
+	WSW(-0.0003*cos(toRadians(22.5)),-0.0003*sin(toRadians(22.5))),
+	W(-0.0003,0),
+	
+	WNW(-0.0003*cos(toRadians(22.5)),0.0003*sin(toRadians(22.5))),
+	NW(-0.0003*cos(toRadians(45)),0.0003*sin(toRadians(45))),
+	NNW(-0.0003*cos(toRadians(67.5)),0.0003*sin(toRadians(67.5)));
 	
 	private double w; 
 	private double h;
--- src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -28,14 +28,14 @@ public class Position {
 		//    --if a point is within the square or not
 		//bl = bottom left
 		//tr = top right
-		Float bl_lat  = 55.942617;
-		Float bl_long = -3.192473;
-		Float tr_lat  = 55.946233;
-		Float tr_long = -3.184319;
+		double bl_lat  = 55.942617;
+		double bl_long = -3.192473;
+		double tr_lat  = 55.946233;
+		double tr_long = -3.184319;
 		
 		//new variables for the current position to make the algorithm more readable
-        Float lat = this.latitude;
-        Float lon = this.longitude;
+        double lat = this.latitude;
+        double lon = this.longitude;
 
         if (lat > bl_lat && lat < tr_lat && lon > bl_long && lon < tr_long ){
             return true;
