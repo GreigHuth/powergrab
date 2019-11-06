c2b760d5487165cbd15ac80ca05337b1e86143a7
Greigashaska <greighuth@hotmail.co.uk>
2019-11-06 15:35:32 +0000

    implemented argument handling, started parsing

--- src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -3,6 +3,9 @@ package uk.ac.ed.inf.powergrab;
 import java.io.*;
 import  java.net.*;
 import org.apache.commons.io.IOUtils;
+import org.javatuples.*;
+
+import com.mapbox.geojson.FeatureCollection;
 
 public class App {
 	
@@ -20,39 +23,53 @@ public class App {
 	
 	
 	public static void main (String args[]) throws MalformedURLException, IOException {
-		// split args into own variables to aid reabability
+		// split arguments into own variables to aid readability
+	    String d = args[0];
+	    String m = args[1];
+	    String y = args[2];
+	    Double lat = Double.parseDouble(args[3]);
+	    Double lon = Double.parseDouble(args[4]);
+	    Pair<Double,Double> startCoords = Pair.with(lat,lon);
+		String seed = args[5];
+		String version = args[6];
 		
-		//variable to hold URL string for the map  
-		String mapString = "http://www.homepages.inf.ed.ac.uk/stg/powergrab/2019/01/01/powergrabmap.geojson";
+		String mapSource = getMapSource(d,m,y);
+		System.out.println(mapSource);
 		
-		//create URL
-		URL mapURL = new URL(mapString);
-		//open URL connection
-		URLConnection conn = mapURL.openConnection();
-		//cast to a http URL
-		HttpURLConnection connHttp = (HttpURLConnection) conn;
-		
-		//setup URL connection
-		connHttp.setReadTimeout(10000); //milliseconds
-		connHttp.setConnectTimeout(15000); // milliseconds
-		connHttp.setRequestMethod("GET"); //sets Http method to GET
-		connHttp.setDoInput(true); //set URL connection type to input
-		connHttp.connect(); // finally connects to the URL
-		
-		//read json data from url
-		InputStream in = connHttp.getInputStream(); //get input stream from url
-		StringWriter writer = new StringWriter();  
-		IOUtils.copy(in, writer, "UTF-8"); 
-		String mapSource = writer.toString();
+		//start parsing the geojson
+		FeatureCollection map = FeatureCollection.fromJson(mapSource);
 		
 		
 		
 		
 		
-		
-		
-		
-		
+	}
+	
+	public static String getMapSource(String d,String m, String y) throws MalformedURLException, IOException {
+	    String mapString = "http://www.homepages.inf.ed.ac.uk/stg/powergrab/"+y+"/"+m+"/"+d+"/powergrabmap.geojson";
+        
+        //create URL
+        URL mapURL = new URL(mapString);
+        //open URL connection
+        URLConnection conn = mapURL.openConnection();
+        //cast to a http URL
+        HttpURLConnection connHttp = (HttpURLConnection) conn;
+        
+        //setup URL connection
+        connHttp.setReadTimeout(10000); //milliseconds
+        connHttp.setConnectTimeout(15000); // milliseconds
+        connHttp.setRequestMethod("GET"); //sets Http method to GET
+        connHttp.setDoInput(true); //set URL connection type to input
+        connHttp.connect(); // finally connects to the URL
+        
+        //read json data from url
+        InputStream in = connHttp.getInputStream(); //get input stream from url
+        StringWriter writer = new StringWriter();  
+        IOUtils.copy(in, writer, "UTF-8"); 
+        String mapSource = writer.toString();
+        
+        return mapSource;
+	    
 	}
 	
 	
